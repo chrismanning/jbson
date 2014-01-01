4465009e1ff6b8b13fcdafc1f97fcfa0ebc10c59
Christian Manning <cmanning999@gmail.com>
2014-01-01 02:18:31 +0000

    Implement json creation from document/array

---
+++ include/jbson/json_writer.hpp
@@ -0,0 +1,196 @@
+//          Copyright Christian Manning 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef JBSON_JSON_WRITER_HPP
+#define JBSON_JSON_WRITER_HPP
+
+#include <string>
+#include <iterator>
+
+#include <boost/utility/string_ref.hpp>
+#include <boost/range/as_literal.hpp>
+#include <boost/concept_check.hpp>
+
+#include "element.hpp"
+#include "document.hpp"
+#include "detail/visit.hpp"
+
+namespace jbson {
+
+struct json_writer;
+
+template <typename OutputIterator, typename Container>
+std::decay_t<OutputIterator> write_json(OutputIterator, const basic_array<Container>&);
+template <typename OutputIterator, typename Container>
+std::decay_t<OutputIterator> write_json(OutputIterator, const basic_document<Container>&);
+
+namespace detail {
+
+template <element_type EType, typename Element, typename OutputIterator> struct json_element_visitor;
+
+namespace {
+
+template <typename T, typename OutputIterator>
+std::decay_t<OutputIterator> stringify(T&& v, OutputIterator out,
+                                       std::enable_if_t<std::is_arithmetic<std::decay_t<T>>::value>* = nullptr) {
+    if(std::is_same<std::decay_t<T>, bool>::value)
+        out = boost::range::copy(boost::as_literal(!!v ? "true" : "false"), out);
+    else
+        out = boost::range::copy(std::to_string(v), out);
+    return out;
+}
+
+template <typename OutputIterator> std::decay_t<OutputIterator> stringify(boost::string_ref v, OutputIterator out) {
+    *out++ = '"';
+    out = boost::range::copy(v, out);
+    *out++ = '"';
+    return out;
+}
+
+template <typename C, typename EC, typename OutputIterator>
+std::decay_t<OutputIterator> stringify(const basic_document<C, EC>& doc, OutputIterator out) {
+    out = boost::range::copy(boost::as_literal("{ "), out);
+
+    auto end = doc.end();
+    for(auto it = doc.begin(); it != end;) {
+        *out++ = '"';
+        out = boost::range::copy(it->name(), out);
+        *out++ = '"';
+        out = boost::range::copy(boost::as_literal(" : "), out);
+        out = detail::visit<detail::json_element_visitor>(it->type(), *it, out);
+        if(++it != end)
+            out = boost::range::copy(boost::as_literal(", "), out);
+    }
+
+    return boost::range::copy(boost::as_literal(" }"), out);
+}
+
+template <typename C, typename EC, typename OutputIterator>
+std::decay_t<OutputIterator> stringify(const basic_array<C, EC>& arr, OutputIterator out) {
+    out = boost::range::copy(boost::as_literal("[ "), out);
+
+    auto end = arr.end();
+    for(auto it = arr.begin(); it != end;) {
+        out = detail::visit<detail::json_element_visitor>(it->type(), *it, out);
+        if(++it != end)
+            out = boost::range::copy(boost::as_literal(", "), out);
+    }
+
+    return boost::range::copy(boost::as_literal(" ]"), out);
+}
+
+} // namespace
+
+template <element_type EType, typename Element, typename OutputIterator> struct json_element_visitor {
+    static_assert(detail::is_element<std::decay_t<Element>>::value, "");
+    BOOST_CONCEPT_ASSERT((boost::OutputIterator<OutputIterator, char>));
+
+    json_element_visitor() = default;
+
+    std::decay_t<OutputIterator> operator()(Element&& e, OutputIterator out) const {
+        return stringify(get<EType>(e), out);
+    }
+};
+
+// oid
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::oid_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        auto oid = get<element_type::oid_element>(e);
+        std::array<char, 25> buf;
+        for(auto i = 0; i < oid.size(); ++i)
+            std::snprintf(&buf[i*2], 3, "%x", static_cast<unsigned char>(oid[i]));
+        return stringify(static_cast<document>(builder("$oid", element_type::string_element,
+                                                       boost::string_ref(buf.data(), 24))), out);
+    }
+};
+
+// db pointer
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::db_pointer_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        using string_type = decltype(get<element_type::string_element>(e));
+        string_type ref;
+        using oid_type = decltype(get<element_type::oid_element>(e));
+        oid_type oid;
+        std::tie(ref, oid) = get<element_type::db_pointer_element>(e);
+        return stringify(static_cast<document>(
+                             builder("$ref", element_type::string_element, ref)("$id", element_type::oid_element, oid)),
+                         out);
+    }
+};
+
+// date
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::date_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        auto date = get<element_type::date_element>(e);
+        return stringify(date.time_since_epoch().count(), out);
+    }
+};
+
+// regex
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::regex_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        using string_type = decltype(get<element_type::string_element>(e));
+        string_type regex, options;
+        std::tie(regex, options) = get<element_type::regex_element>(e);
+        return stringify(static_cast<document>(builder("$regex", element_type::string_element,
+                                                       regex)("$options", element_type::string_element, options)),
+                         out);
+    }
+};
+
+// scoped code
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::scoped_javascript_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const { return out; }
+};
+
+// voids
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::null_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        return boost::range::copy(boost::as_literal("null"), out);
+    }
+};
+
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::undefined_element, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        return boost::range::copy(boost::as_literal("null"), out);
+    }
+};
+
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::max_key, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        return boost::range::copy(boost::as_literal("null"), out);
+    }
+};
+
+template <typename Element, typename OutputIterator>
+struct json_element_visitor<element_type::min_key, Element, OutputIterator> {
+    std::decay_t<OutputIterator> operator()(Element&& e, std::decay_t<OutputIterator> out) const {
+        return boost::range::copy(boost::as_literal("null"), out);
+    }
+};
+
+} // namespace detail
+
+template <typename OutputIterator, typename Container>
+std::decay_t<OutputIterator> write_json(const basic_array<Container>& arr, OutputIterator out) {
+    return detail::stringify(arr, out);
+}
+
+template <typename OutputIterator, typename Container>
+std::decay_t<OutputIterator> write_json(const basic_document<Container>& doc, OutputIterator out) {
+    return detail::stringify(doc, out);
+}
+
+} // namespace jbson
+
+#endif // JBSON_JSON_WRITER_HPP
---
+++ test/json_writer_test.cpp
@@ -0,0 +1,150 @@
+//          Copyright Christian Manning 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <string>
+using namespace std::literals;
+
+#include <boost/exception/diagnostic_information.hpp>
+#include <boost/range/as_array.hpp>
+
+#include <jbson/json_reader.hpp>
+#include <jbson/json_writer.hpp>
+#include <jbson/builder.hpp>
+using namespace jbson;
+
+#include <gtest/gtest.h>
+
+TEST(JsonWriterTest, StringifyTest0) {
+    auto json = std::string{};
+    detail::stringify("some string", std::back_inserter(json));
+    EXPECT_EQ(R"("some string")", json);
+}
+
+TEST(JsonWriterTest, StringifyTest1) {
+    auto json = std::string{};
+    auto doc = document{R"({ "hello"
+               : "world" })"_json};
+    detail::stringify(doc, std::back_inserter(json));
+    EXPECT_EQ(R"({ "hello" : "world" })", json);
+}
+
+TEST(JsonWriterTest, StringifyTest2) {
+    auto json = std::string{};
+    auto arr = array{R"(["hello"   , "world" ])"_json};
+    detail::stringify(arr, std::back_inserter(json));
+    EXPECT_EQ(R"([ "hello", "world" ])", json);
+}
+
+TEST(JsonWriterTest, StringifyTest3) {
+    auto json = std::string{};
+    detail::stringify(123, std::back_inserter(json));
+    EXPECT_EQ("123", json);
+}
+
+TEST(JsonWriterTest, StringifyTest4) {
+    auto json = std::string{};
+    detail::stringify(0, std::back_inserter(json));
+    EXPECT_EQ("0", json);
+}
+
+TEST(JsonWriterTest, StringifyTest5) {
+    auto json = std::string{};
+    detail::stringify(false, std::back_inserter(json));
+    EXPECT_EQ("false", json);
+    json.clear();
+    detail::stringify(true, std::back_inserter(json));
+    EXPECT_EQ("true", json);
+}
+
+TEST(JsonWriterTest, StringifyTest6) {
+    auto json = std::string{};
+    detail::stringify(4.543210, std::back_inserter(json));
+    EXPECT_EQ("4.543210", json);
+}
+
+TEST(JsonWriterTest, JsonWriteTest1) {
+    auto json = std::string{};
+    auto doc = document{R"({ "hello" : "world" })"_json};
+    write_json(doc, std::back_inserter(json));
+
+    EXPECT_EQ(R"({ "hello" : "world" })", json);
+}
+
+TEST(JsonWriterTest, JsonWriteTest2) {
+    auto json = std::string{};
+    write_json(static_cast<document>(builder("some value", element_type::int32_element, 123)
+               ("some other value", element_type::boolean_element, true)),
+               std::back_inserter(json));
+
+    EXPECT_EQ(R"({ "some other value" : true, "some value" : 123 })", json);
+}
+
+TEST(JsonWriterTest, JsonWriteTest3) {
+    auto json = std::array<char, 22>{};
+    auto doc = document{R"({ "hello" : "world" })"_json};
+    write_json(doc, json.begin());
+
+    EXPECT_EQ(boost::as_array(R"({ "hello" : "world" })"), json);
+}
+
+std::string demangle(boost::string_ref t) {
+    const auto str = abi::__cxa_demangle(t.data(), nullptr, nullptr, nullptr);
+    std::string out;
+    if(str) {
+        out = str;
+        ::free(str);
+    }
+    return std::move(out);
+}
+
+TEST(JsonWriterTest, JsonWriteTest4) {
+    auto json = std::string{};
+    write_json(static_cast<document>(builder("some value", element_type::int32_element, 123)
+               ("some regex", element_type::regex_element, std::make_tuple(".*", "i"))),
+               std::back_inserter(json));
+
+    EXPECT_EQ(R"({ "some regex" : { "$options" : "i", "$regex" : ".*" }, "some value" : 123 })", json);
+}
+
+TEST(JsonWriterTest, JsonWriteTest5) {
+    auto json = std::string{};
+    std::array<char, 12> oid{{static_cast<char>(0x50),
+                    static_cast<char>(0x7f),
+                    static_cast<char>(0x1f),
+                    static_cast<char>(0x77),
+                    static_cast<char>(0xbc),
+                    static_cast<char>(0xf8),
+                    static_cast<char>(0x6c),
+                    static_cast<char>(0xd7),
+                    static_cast<char>(0x99),
+                    static_cast<char>(0x43),
+                    static_cast<char>(0x90),
+                    static_cast<char>(0x11)
+                             }};
+    write_json(static_cast<document>(builder("some oid", element_type::oid_element, oid)),
+               std::back_inserter(json));
+    EXPECT_EQ(R"({ "some oid" : { "$oid" : "507f1f77bcf86cd799439011" } })", json);
+}
+
+TEST(JsonWriterTest, JsonWriteTest6) {
+    auto json = std::string{};
+    std::array<char, 12> oid{{static_cast<char>(0x50),
+                    static_cast<char>(0x7f),
+                    static_cast<char>(0x1f),
+                    static_cast<char>(0x77),
+                    static_cast<char>(0xbc),
+                    static_cast<char>(0xf8),
+                    static_cast<char>(0x6c),
+                    static_cast<char>(0xd7),
+                    static_cast<char>(0x99),
+                    static_cast<char>(0x43),
+                    static_cast<char>(0x90),
+                    static_cast<char>(0x11)
+                             }};
+    write_json(static_cast<document>(builder("some ref", element_type::db_pointer_element,
+                                                                       std::make_tuple("some collection", oid))),
+               std::back_inserter(json));
+    EXPECT_EQ(R"({ "some ref" : { "$id" : { "$oid" : "507f1f77bcf86cd799439011" }, "$ref" : "some collection" } })", json);
+}
