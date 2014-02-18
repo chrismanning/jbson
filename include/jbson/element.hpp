7cebf894aef1d38188027293ed3050bd384e84e4
Christian Manning <cmanning999@gmail.com>
2014-02-18 18:38:42 +0000

    Fix local string compare

--- include/jbson/element.hpp
+++ include/jbson/element.hpp
@@ -203,7 +203,8 @@ template <class Container> struct basic_element {
             if(type() == element_type::double_element)
                 return value<double>() < other.value<double>();
             if(type() == element_type::string_element)
-                return ::strcoll(get<element_type::string_element>(*this).data(), get<element_type::string_element>(other).data());
+                return ::strcoll(get<element_type::string_element>(*this).data(),
+                                 get<element_type::string_element>(other).data()) < 0;
             return m_data < other.m_data;
         }
         return res < 0;
