b73458c77b2eb9828389dd794ab00d4d269e5eaa
Christian Manning <cmanning999@gmail.com>
2014-08-23 15:19:57 +0100

    Use "C" number punct locale to be more IDE friendly.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -7,7 +7,7 @@
 
 GTEST_API_ int main(int argc, char **argv) {
     std::locale loc{"en_GB.UTF-8"};
-    std::locale::global(loc);
+    std::locale::global(loc.combine<std::numpunct<char>>(std::locale("C")));
     loc = std::locale{};
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
